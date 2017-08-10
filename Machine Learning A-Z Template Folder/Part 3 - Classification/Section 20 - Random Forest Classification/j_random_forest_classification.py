047cec232e4770242381d2457e77130b33f150e4
gdi <jigar.gandhi@bauer.de>
2017-08-10 16:26:24 +0530

    Random forest

--- Machine Learning A-Z Template Folder/Part 3 - Classification/Section 20 - Random Forest Classification/j_random_forest_classification.py
+++ Machine Learning A-Z Template Folder/Part 3 - Classification/Section 20 - Random Forest Classification/j_random_forest_classification.py
@@ -1,3 +1,4 @@
+# Classification template
 
 # Importing the libraries
 import numpy as np
@@ -9,7 +10,6 @@ dataset = pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
 
-
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
@@ -19,11 +19,12 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
-#Fitting Naive Bayes to the Training set
-from sklearn.naive_bayes import GaussianNB
-classifier = GaussianNB()
-classifier.fit(X_train, y_train)
 
+# Fitting classifier to the Training set
+# Create your classifier here
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(criterion='entropy', random_state=0)
+classifier.fit(X_train, y_train)
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
@@ -43,7 +44,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Naive Bayes (Training set)')
+plt.title('Random Forest (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
@@ -61,8 +62,9 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Naive Bayes (Test set)')
+plt.title('Random Forest (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
 plt.show()
+
---
+++ Machine Learning A-Z Template Folder/Part 3 - Classification/Section 21 - Evaluating Classification Models Performance/Notes.md
@@ -0,0 +1,16 @@
+False positive: Items falsly classified as positive, Predict an effect that did not occur
+
+False negative items falsly classfied as negative, Did not predict an event that occurred
+
+
+Predicted Actual
+
+	 | True |	false
+True |   A  |  FP 1
+False|  FN 2|	D
+
+Actual Predicted
+
+	 | True |	false
+True |   A  |  False Positive 1
+False|  FN 2|	D
